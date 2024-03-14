06891af41af1f7f1b779977410fd49d19a6fbcd0
repiyann <muhrepiyan26@gmail.com>
2024-03-14 14:22:12 +0700

    fix(provider): fix theme provider persist when refresh

--- frontend/src/components/themeProvider.tsx
+++ frontend/src/components/themeProvider.tsx
@@ -1,51 +1,3 @@
-// import { createContext, useEffect, useState } from 'react'
-
-// type Theme = 'light' | 'dark' | 'system'
-
-// export interface ThemeContextType {
-// 	theme: Theme
-// 	setTheme: (newTheme: Theme) => void
-// }
-
-// export const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
-
-// export function ThemeProvider({ children }: { children: React.ReactNode }) {
-// 	const [theme, setThemeState] = useState<Theme>(() => {
-// 		const storedTheme = localStorage.getItem('theme') as Theme
-// 		return (['light', 'dark'].includes(storedTheme) ? storedTheme : 'system') || 'system'
-// 	})
-
-// 	function setTheme(newTheme: Theme) {
-// 		localStorage.setItem('theme', newTheme)
-// 		setThemeState(newTheme)
-// 	}
-
-// 	useEffect(() => {
-// 		const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-// 		const listener = () => {
-// 			if (theme === 'system') {
-// 				setTheme(mediaQuery.matches ? 'dark' : 'light')
-// 			}
-// 		}
-// 		listener()
-// 		mediaQuery.addEventListener('change', listener)
-// 		return () => mediaQuery.removeEventListener('change', listener)
-// 	}, [theme])
-
-// 	useEffect(() => {
-// 		const html = document.documentElement
-// 		html.classList.remove('light', 'dark')
-// 		html.classList.add(theme)
-// 	}, [theme])
-
-// 	const contextValue: ThemeContextType = {
-// 		theme,
-// 		setTheme
-// 	}
-
-// 	return <ThemeContext.Provider value={contextValue}>{children}</ThemeContext.Provider>
-// }
-
 import { createContext, useEffect, useState } from 'react'
 
 type Theme = 'light' | 'dark' | 'system'
@@ -58,13 +10,22 @@ export interface ThemeContextType {
 export const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-	const [theme, setThemeState] = useState<Theme>('system')
-	const [actualTheme, setActualTheme] = useState<Theme>('light')
+	const [theme, setThemeState] = useState<Theme>(() => {
+		const savedTheme = localStorage.getItem('theme')
+		return (savedTheme as Theme) || 'system'
+	})
+	const [actualTheme, setActualTheme] = useState<Theme>(() => {
+		const savedTheme = localStorage.getItem('theme')
+		return (savedTheme as Theme) || 'system'
+	})
 
 	function setTheme(newTheme: Theme) {
 		setThemeState(newTheme)
 		if (newTheme !== 'system') {
 			setActualTheme(newTheme)
+			localStorage.setItem('theme', newTheme)
+		} else {
+			localStorage.removeItem('theme')
 		}
 	}
 
