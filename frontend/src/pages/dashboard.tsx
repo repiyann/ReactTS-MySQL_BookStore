826b5af68379d06f7cc6cbca1e45e68819d2b860
repiyann <muhrepiyan26@gmail.com>
2024-03-14 13:14:32 +0700

    feat(dashboard); add a dashboard page

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,10 +1,11 @@
-function Dashboard() {
+import Navbar from '../components/navbar'
+
+function dashboard() {
 	return (
-		<div className="px-36 py-5 bg-gray-500 text-lg">
-			<div>Navbar</div>
-			<div className="flex justify-between">Login</div>
+		<div>
+			<Navbar />
 		</div>
 	)
 }
 
-export default Dashboard
+export default dashboard
